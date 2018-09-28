cee0c42f59612a12e6a682f8948534bd17a04c24
CristianZatt <cristian.zatt@gmail.com>
2018-09-28 20:06:23 -0300

    banchmarks

--- main5.cpp
+++ main5.cpp
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <omp.h>
 
-int max_threads;
-
+int max_threads, gloops = 0;
 
 int **multiplySquareMatix(int **matrix1, int length1, int **matrix2, int length2, int tipo);
 
@@ -15,15 +14,15 @@ void freeSquareMatrix(int **matrix);
 
 void printBanchmarkMultiply(int tipo, double start, double t_criacao, double end, int count);
 
-void printBanchmarkSum(int tipo, double start, double end);
+void printBanchmarkSum(double sumResult, int tipo, double start, double end);
 
-int **addMatixValues(int **matrix, int length, int tipo);
+double sumMatrixValues(int **matrix, int length, int tipo);
 
 int main(){
 	double start = 0, end = 0, t_criacao;
 	int **matrix1 = NULL, **matrix2 = NULL, **result = NULL;
 	int length, max = 1, tipo;
-	int i;
+	int i, y = 0;
 	int count;
 
 	fputs("Tamanho da matriz:  ", stdout);
@@ -34,48 +33,49 @@ int main(){
 	scanf("%d", &count);
 	getchar();
 
-	fputs("Numero de threads: ", stdout);
+	fputs("Numero maximo de threads: ", stdout);
 	scanf("%d", &max_threads);
 	getchar();
-	omp_set_num_threads(max_threads);
-	
-	for (tipo = 0; tipo < 4; i++){
-		start = omp_get_wtime();
-		matrix1 = createSquareMatrix(matrix1, length, max);	matrix2 = createSquareMatrix(matrix2, length, max);
-		t_criacao = omp_get_wtime();
-		
-		for (i = 0; i < count; i++) result = multiplySquareMatix(matrix1, length, matrix2, length, tipo);
+	for (int t = 2; t <= max_threads; t++){
+		omp_set_num_threads(t);
+		printf("\n +++++++++++\n THREADS: %i", t);
+		for (tipo = y; tipo <= 6; tipo++){
+			start = omp_get_wtime();
+			matrix1 = createSquareMatrix(matrix1, length, max);	matrix2 = createSquareMatrix(matrix2, length, max);
+			t_criacao = omp_get_wtime();
+			
+			for (i = 0; i < count; i++) result = multiplySquareMatix(matrix1, length, matrix2, length, tipo);
 
-		end = omp_get_wtime();
-		
-		printBanchmarkMultiply(tipo, start, t_criacao, end, count);
+			end = omp_get_wtime();
+			
+			printBanchmarkMultiply(tipo, start, t_criacao, end, count);
+		}
+
+		for (tipo = y; tipo <= 2; tipo++){
+			double sumResult = 0;
+			start = omp_get_wtime();
+			sumResult = sumMatrixValues(result, length, tipo);
+			end = omp_get_wtime();
+			printBanchmarkSum(sumResult, tipo, start, end);
+		}
+		y = 1;
 	}
-	
-	end = omp_get_wtime();
-	
-	for (tipo = 0; tipo < 1; i++){
-		start = omp_get_wtime();
-		addMatixValues(result, length, tipo);
-		end = omp_get_wtime();
-		printBanchmarkSum(tipo, start, end);
-	}
-	
-	
 	freeSquareMatrix(matrix1); freeSquareMatrix(matrix2); freeSquareMatrix(result);
 	return 0;
 }
 
 void printBanchmarkMultiply(int tipo, double start, double t_criacao, double end, int count){
 	printf("\n MTP Tipo: %i", tipo);
-	printf("\nTime of creation : %.16f sec", t_criacao - start);
-	printf("\nAverage elasped time of one multiplication : %.16f sec", (end - t_criacao) / count);
-	printf("\nTotal time of multiplications : %.16f sec", end - t_criacao);
-	printf("\nTotal time of execution : %.16f sec\n", end - start);
+	printf("\nTempo de criacao : %.16f sec", t_criacao - start);
+	printf("\nTempo medio de uma multiplicacao : %.16f sec", (end - t_criacao) / count);
+	printf("\nTempo total das multiplicacoes : %.16f sec", end - t_criacao);
+	printf("\nTempo total de execucao : %.16f sec\n", end - start);
 }
 
-void printBanchmarkSum(int tipo, double start, double end){
+void printBanchmarkSum(double sumResult, int tipo, double start, double end){
 	printf("\n SUM Tipo: %i", tipo);
-	printf("\nTime of sum : %.16f sec\n", end - start);
+	printf("\n Resultado: %i", sumResult);
+	printf("\nTempo da soma : %.16f sec\n", end - start);
 }
 
 
@@ -91,27 +91,51 @@ int **multiplySquareMatix(int **matrix1, int length1, int **matrix2, int length2
 		for (i = 0; i < length1; i++)for (j = 0; j < length2; j++)for (k = 0; k < length1; k++)result[i][j] += matrix1[i][k] * matrix2[k][j];
 	}
 	if(tipo == 2){
-		#pragma omp parallel for schedule(dynamic) collapse(1) private(i,j,k)shared(matrix1,matrix2,result)
+		#pragma omp parallel for schedule(static) collapse(2) private(i,j,k)shared(matrix1,matrix2,result)
 		for (i = 0; i < length1; i++)for (j = 0; j < length2; j++)for (k = 0; k < length1; k++)result[i][j] += matrix1[i][k] * matrix2[k][j];
 	}
 	if(tipo == 3){
-		#pragma omp parallel for schedule(dynamic) collapse(2) private(i,j,k)shared(matrix1,matrix2,result)
+		#pragma omp parallel for schedule(static) collapse(3) private(i,j,k)shared(matrix1,matrix2,result)
 		for (i = 0; i < length1; i++)for (j = 0; j < length2; j++)for (k = 0; k < length1; k++)result[i][j] += matrix1[i][k] * matrix2[k][j];
 	}
 	if(tipo == 4){
+		#pragma omp parallel for schedule(dynamic) private(i,j,k)shared(matrix1,matrix2,result)
+		for (i = 0; i < length1; i++)for (j = 0; j < length2; j++)for (k = 0; k < length1; k++)result[i][j] += matrix1[i][k] * matrix2[k][j];
+	}
+	if(tipo == 5){
+		#pragma omp parallel for schedule(dynamic) collapse(2) private(i,j,k)shared(matrix1,matrix2,result)
+		for (i = 0; i < length1; i++)for (j = 0; j < length2; j++)for (k = 0; k < length1; k++)result[i][j] += matrix1[i][k] * matrix2[k][j];
+	}
+	if(tipo == 6){
 		#pragma omp parallel for schedule(dynamic) collapse(3) private(i,j,k)shared(matrix1,matrix2,result)
 		for (i = 0; i < length1; i++)for (j = 0; j < length2; j++)for (k = 0; k < length1; k++)result[i][j] += matrix1[i][k] * matrix2[k][j];
 	}
 	return result;
 }
 
-int **addMatixValues(int **matrix, int length, int tipo){
+double sumMatrixValues(int **matrix, int length, int tipo){
 	int i, j;
-	int sum = 0;
+	double sum = 0, x = 0;
 	if(tipo == 0){
-		#pragma omp parallel for reduction (+:sum)
-		for (i=0;i<length;i++)for (j=0;j<length;j++)sum+=matrix[i][j];
+		for (i=0;i<length;i++)for(j=0;j<length;j++)sum+=matrix[i][j];
 	}
+	if(tipo == 1){
+		#pragma omp parallel for reduction (+:sum) collapse(2)
+		for (i=0;i<length;i++)for(j=0;j<length;j++)sum+=matrix[i][j];
+	}
+	if(tipo == 2){
+		#pragma omp parallel for reduction (+:sum) collapse(2)
+		for (i=0;i<length;i++){
+			for(j=0;j<length;j++){
+				sum+=matrix[i][j];
+				#pragma omp critical
+				{
+					gloops = gloops + omp_get_thread_num();
+				}
+			}
+		}
+	}
+	return sum;
 }
 
 void showSquareMatrix(int **matrix, int length){
